1ac217c4f510978f9f01fabb95d76c249041bac4
UncleVasya (Oleg Ovcharenko) <unclevasyaai@gmail.com>
2018-05-01 13:35:04 +0300

    Patching pops_api.dll just in case, like @Habaus suggested

--- bin/xp_CK2.cmd
+++ bin/xp_CK2.cmd
@@ -1,2 +1,3 @@
 @echo off
 rundll32 zernel32.dll,PatchFile CK2game.exe
+rundll32 zernel32.dll,PatchFile pops_api.dll
--- bin/xp_HoI4.cmd
+++ bin/xp_HoI4.cmd
@@ -1,2 +1,3 @@
 @echo off
 rundll32 zernel32.dll,PatchFile hoi4.exe
+rundll32 zernel32.dll,PatchFile pops_api.dll
